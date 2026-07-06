fe280ecd6eba168ebb80b0bbbcbfce37f4c7f107
Muawwiz <muawwizraza2023@gmail.com>
2026-07-06 16:28:08 +0500

    log fruits

--- math_game/app.js
+++ math_game/app.js
@@ -1,3 +1,8 @@
 // today we create math game
 
 console.log("math game");
+
+let fruits = ["Mango", "Banana", "Apple"];
+fruits.splice(2, 0, "kivi");
+fruits.splice(1,1, "Cheeku");
+console.log(fruits);
